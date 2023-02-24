a8d6c78f1bef957e73d4d640cdd4a3e883a06d03
Andres Henao <user.email andres_henao@elpoli.edu.co>
2023-02-23 19:01:31 -0500

    metodo de suma

--- src/co/edu/poli/ces3/Estudiante.java
+++ src/co/edu/poli/ces3/Estudiante.java
@@ -3,5 +3,10 @@ package co.edu.poli.ces3;
 public class Estudiante {
     public static void main(String[] args) {
         System.out.println("HI");
+
     }
+    public static int sumar(int a, int b){
+        return a + b;
+    }
+
 }
